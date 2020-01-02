bf2150b557c9e6d138f82c4219def6cbc0c361bd
OlegBalabukh <olegukoff@gmail.com>
2020-01-02 02:10:09 +0200

    code refactoring before adding router

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Navbar } from './components/Navbar';
-import { TodoForm } from './components/TodoForm';
-import { TodoList } from './components/TodoList';
-import { ITodo } from './interfaces';
-
-declare var confirm: (question: string) => boolean
 
 const App: React.FC = () => {
-  const [todos, setTodos] = useState<ITodo[]>([]); // or useState<Array<ITodo>>
-
-  useEffect(() => {
-    const saved = JSON.parse(localStorage.getItem('todos') || '[]') as ITodo[]
-    setTodos(saved)
-  }, [])
-
-  useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos))
-  }, [todos])
-
-  const addHandler = (title: string)=> {
-    const newTodo: ITodo = {
-      title: title,
-      id: Date.now(),
-      completed: false
-    }
-    // setTodos([newTodo, ...todos])
-    setTodos(prev => [newTodo, ...prev])
-  }
-
-  const toggleHandler = (id: number) => {
-    setTodos(prev => 
-      prev.map(todo => {
-        if (todo.id === id) {
-          todo.completed = !todo.completed
-        }
-        return todo
-      })
-    )
-  }
-
-  const removeHandler = (id: number) => {
-    // const shouldRemove = window.confirm('Are you sure you want to delete this element?')
-    const shouldRemove = confirm('Are you sure you want to delete this element?')
-    if (shouldRemove) {
-      setTodos(prev => prev.filter(todo => todo.id !== id))
-    }
-  }
-
   return (
     <>
       <Navbar />
-      <div className="container">
-        <TodoForm onAdd={addHandler} />
-
-        <TodoList
-          todos={todos}
-          onToggle={toggleHandler}
-          onRemove={removeHandler}
-        />
-      </div>
+      <div className="container"></div>
     </>
   );
 }
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const AboutPage: React.FC = () => {
+  return (
+    <>
+      <h1>Information page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. At esse non
+        obcaecati ut quos quam aut eos deleniti, in totam.
+      </p>
+      <button className="btn">Back to List of Todos</button>
+    </>
+  );
+}
